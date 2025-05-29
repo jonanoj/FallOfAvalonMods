a0a38f3d3811317cba44e99e185e12b4ae70f912
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-05-30 02:36:53 +0300

    Generate FastTravelAlways mod from template

---
+++ FastTravelAlways/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace FastTravelAlways;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.FastTravelAlways";
+    public const string PLUGIN_NAME = "FastTravelAlways";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
