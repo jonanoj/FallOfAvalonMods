8a766f62795bff9de030c493c8f88ed516e207ed
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-05-31 23:17:27 +0300

    Generate FallDamageControl project

---
+++ FallDamageControl/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace FallDamageControl;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.FallDamageControl";
+    public const string PLUGIN_NAME = "FallDamageControl";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
