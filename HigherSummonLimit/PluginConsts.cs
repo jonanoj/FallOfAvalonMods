6422563205269ff7cf461572afdaf46693dc4a60
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-06-02 00:45:56 +0300

    Generate HigherSummonLimit project

---
+++ HigherSummonLimit/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace HigherSummonLimit;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.HigherSummonLimit";
+    public const string PLUGIN_NAME = "HigherSummonLimit";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
