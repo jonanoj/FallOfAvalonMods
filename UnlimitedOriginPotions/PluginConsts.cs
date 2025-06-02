0fe34e5372a79ede4539063daf348c4d78c6b6ba
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-06-02 18:25:37 +0300

    Generate UnlimitedOriginPotions project

---
+++ UnlimitedOriginPotions/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace UnlimitedOriginPotions;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.UnlimitedOriginPotions";
+    public const string PLUGIN_NAME = "UnlimitedOriginPotions";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
