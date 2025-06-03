763ada3e70d072357c747b207f6678f401ff5b08
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-06-04 00:09:51 +0300

    Generate LessSlowMotion project

---
+++ LessSlowMotion/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace LessSlowMotion;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.LessSlowMotion";
+    public const string PLUGIN_NAME = "LessSlowMotion";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
