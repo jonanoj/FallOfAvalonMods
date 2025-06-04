ecc6651c78a4d1c4b772dc67208a1c69af58ddf0
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-06-04 21:36:18 +0300

    Generate DisableNightGlow project

---
+++ DisableNightGlow/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace DisableNightGlow;
+
+[BepInPlugin(PluginConsts.PLUGIN_GUID, PluginConsts.PLUGIN_NAME, PluginConsts.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+    internal static PluginConfig PluginConfig;
+
+    public Harmony HarmonyInstance { get; set; }
+
+    public override void Load()
+    {
+        Log = base.Log;
+        Log.LogInfo($"Plugin {PluginConsts.PLUGIN_GUID} is loading...");
+
+        PluginConfig = new PluginConfig(Config);
+        HarmonyInstance = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+        Log.LogInfo($"Plugin {PluginConsts.PLUGIN_GUID} is loaded!");
+    }
+
+    public override bool Unload()
+    {
+        Log.LogInfo($"Plugin {PluginConsts.PLUGIN_GUID} is unloading...");
+
+        HarmonyInstance?.UnpatchSelf();
+
+        Log.LogInfo($"Plugin {PluginConsts.PLUGIN_GUID} is unloaded!");
+        return true;
+    }
+}
---
+++ DisableNightGlow/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace DisableNightGlow;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.DisableNightGlow";
+    public const string PLUGIN_NAME = "DisableNightGlow";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
