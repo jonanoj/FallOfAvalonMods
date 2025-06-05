1e76f0940b23bb15b7c59014d42907fdfaed30a9
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-06-06 00:13:16 +0300

    Fix player staying overencumbered after installing the mod when under effect

---
+++ WeightControl/HeroPatch.cs
@@ -0,0 +1,68 @@
+using System.Diagnostics.CodeAnalysis;
+using Awaken.TG.Main.Heroes;
+using Awaken.TG.Main.Heroes.Statuses;
+using Awaken.TG.Main.Templates;
+using Awaken.TG.MVC;
+using HarmonyLib;
+
+namespace WeightControl;
+
+[HarmonyPatch]
+public class HeroPatch
+{
+    private const string OverencumberedGuid = "61d1845f37ba56a4096a46408ce80ee3"; // Status_Overencumbered
+
+    [HarmonyPatch(typeof(Hero), nameof(Hero.OnFullyInitialized))]
+    [HarmonyPostfix]
+    public static void OnFullyInitializedPostfix(Hero __instance)
+    {
+        if (!TryGetTemplate(OverencumberedGuid, out StatusTemplate overencumberedTemplate))
+        {
+            return;
+        }
+
+        // Remove the overencumbered status if it exists, the game recalculates the encumbrance status after this patch and will re-apply if needed
+        var characterStatuses = __instance.Statuses;
+        var status = characterStatuses.FirstFrom(overencumberedTemplate);
+        if (status != null)
+        {
+#if DEBUG
+            Plugin.Log.LogInfo(
+                $"{nameof(Hero)}.{nameof(Hero.OnFullyInitialized)}: Overencumbered status exists, removing it");
+#endif
+            characterStatuses.RemoveStatus(overencumberedTemplate);
+        }
+#if DEBUG
+        else
+        {
+            Plugin.Log.LogInfo(
+                $"{nameof(Hero)}.{nameof(Hero.OnFullyInitialized)}: Overencumbered status does not exist");
+        }
+#endif
+    }
+
+    private static bool TryGetTemplate<T>(string guid, [MaybeNullWhen(false)] out T template)
+        where T : Template
+    {
+        var templatesProvider = World.Services?.Get<TemplatesProvider>();
+        if (templatesProvider == null)
+        {
+            Plugin.Log.LogWarning("TemplatesProvider is not available yet, can't find template details");
+            template = null;
+            return false;
+        }
+
+        template = templatesProvider.Get<T>(guid);
+        if (template == null)
+        {
+            Plugin.Log.LogWarning($"{typeof(T).Name} template with GUID {guid} not found.");
+            return false;
+        }
+
+#if DEBUG
+        Plugin.Log.LogInfo(
+            $"Found {typeof(T).Name} template: {template.GUID} - {template.DebugName} - {template.name}");
+#endif
+        return true;
+    }
+}
