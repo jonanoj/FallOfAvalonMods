114bc0e5e678bb5b046fb57f88f746ffb286c7dc
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-06-06 21:19:19 +0300

    Generate LessHoldTime project

---
+++ LessHoldTime/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace LessHoldTime;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.LessHoldTime";
+    public const string PLUGIN_NAME = "LessHoldTime";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
