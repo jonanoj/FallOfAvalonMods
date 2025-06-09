1069c0f4736469e59ce7d79a3a315614e7e1de23
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-06-09 23:46:02 +0300

    Generate AdditionalInventorySorting project

---
+++ AdditionalInventorySorting/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace AdditionalInventorySorting;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.AdditionalInventorySorting";
+    public const string PLUGIN_NAME = "AdditionalInventorySorting";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
