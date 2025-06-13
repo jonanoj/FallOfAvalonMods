204944dd3caf9529d541fb45e72add960feb4ec5
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-06-14 00:19:31 +0300

    Generate CustomKeybinds project

---
+++ CustomKeybinds/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace CustomKeybinds;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.CustomKeybinds";
+    public const string PLUGIN_NAME = "CustomKeybinds";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
