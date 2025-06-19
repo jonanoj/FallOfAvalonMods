074d5a2eca202a179e8bb4dac2b3a0b750e9898f
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-06-19 23:21:13 +0300

    Generate HigherWeightLimit project

---
+++ HigherWeightLimit/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace HigherWeightLimit;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.HigherWeightLimit";
+    public const string PLUGIN_NAME = "HigherWeightLimit";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
