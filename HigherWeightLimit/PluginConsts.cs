bf6c15896ed6cef7eac09667da1a500816e9a600
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-06-21 13:10:44 +0300

    Bump version to 1.1.0

--- HigherWeightLimit/PluginConsts.cs
+++ HigherWeightLimit/PluginConsts.cs
@@ -4,5 +4,5 @@ public static class PluginConsts
 {
     public const string PLUGIN_GUID = "jonanoj.HigherWeightLimit";
     public const string PLUGIN_NAME = "HigherWeightLimit";
-    public const string PLUGIN_VERSION = "1.0.0";
+    public const string PLUGIN_VERSION = "1.1.0";
 }
