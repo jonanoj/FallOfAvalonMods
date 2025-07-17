86a77ea35445ff9c8fca38853b3989a32f19babd
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-07-17 22:26:17 +0300

    Target BepInEx5

--- templates/mod/Plugin.cs
+++ templates/mod/Plugin.cs
@@ -7,14 +7,14 @@ using HarmonyLib;
 namespace ModName;
 
 [BepInPlugin(PluginConsts.PLUGIN_GUID, PluginConsts.PLUGIN_NAME, PluginConsts.PLUGIN_VERSION)]
-public class Plugin : BasePlugin
+public class Plugin : BaseUnityPlugin
 {
-    internal static new ManualLogSource Log;
+    internal static ManualLogSource Log;
     internal static PluginConfig PluginConfig;
 
     public Harmony HarmonyInstance { get; set; }
 
-    public override void Load()
+    public void Awake()
     {
         Log = base.Log;
         Log.LogInfo($"Plugin {PluginConsts.PLUGIN_GUID} is loading...");
@@ -25,13 +25,12 @@ public class Plugin : BasePlugin
         Log.LogInfo($"Plugin {PluginConsts.PLUGIN_GUID} is loaded!");
     }
 
-    public override bool Unload()
+    public void OnDestroy()
     {
         Log.LogInfo($"Plugin {PluginConsts.PLUGIN_GUID} is unloading...");
 
         HarmonyInstance?.UnpatchSelf();
 
         Log.LogInfo($"Plugin {PluginConsts.PLUGIN_GUID} is unloaded!");
-        return true;
     }
 }
