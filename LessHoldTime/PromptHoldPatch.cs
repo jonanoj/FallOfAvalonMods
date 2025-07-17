627bd5c8e65ed63da5e3cb5e723dad98a5d93b24
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-07-18 01:57:06 +0300

    Migrate to BepInEx5

--- LessHoldTime/PromptHoldPatch.cs
+++ LessHoldTime/PromptHoldPatch.cs
@@ -1,42 +1,61 @@
 using Awaken.TG.Main.UI.ButtonSystem;
 using Awaken.TG.Main.Utility;
+using Awaken.TG.Main.Utility.UI.Keys;
 using HarmonyLib;
 
 namespace LessHoldTime;
 
-[HarmonyPatch(typeof(Prompt), nameof(Prompt.Hold))]
-public class PromptHoldPatch
+[HarmonyPatch]
+public class PromptPatch
 {
-    public static void Postfix(Prompt __result)
+    [HarmonyPatch(typeof(Prompt), MethodType.Constructor, [
+        typeof(KeyBindings),
+        typeof(string),
+        typeof(IButton.PressType),
+        typeof(System.Action),
+        typeof(Prompt.Position),
+        typeof(ControlSchemeFlag),
+        typeof(float)
+    ])]
+    [HarmonyPrefix]
+    public static void PromptConstructorPrefix(
+        [HarmonyArgument(0)] KeyBindings key,
+        [HarmonyArgument(1)] string name,
+        [HarmonyArgument(2)] ref IButton.PressType pressType,
+        [HarmonyArgument(6)] ref float holdTime)
     {
+        if (pressType != IButton.PressType.Hold)
+        {
+            return;
+        }
+
 #if DEBUG
         Plugin.Log.LogInfo($"{nameof(Prompt)}.{nameof(Prompt.Hold)} created. " +
-                           $"keybind={__result._key.ToString()}, name={__result._name}," +
-                           $"holdTime={__result.HoldTime}, actionName={__result.ActionName}");
+                           $"keybind={key}, name={name}, holdTime={holdTime}");
 #endif
 
-        if (__result._key.CompareTo(KeyBindings.UI.Crafting.CraftOne) == 0)
+        if (key.CompareTo(KeyBindings.UI.Crafting.CraftOne) == 0)
         {
-            float originalHoldTime = __result.HoldTime;
+            float originalHoldTime = holdTime;
             float newHoldTime = originalHoldTime * Plugin.PluginConfig.CraftHoldTimeMultiplier.Value;
 
             if (Plugin.PluginConfig.CraftHoldTimeMultiplier.Value <= 0.10f)
             {
                 // Change the UI from hold to tap if it's under a certain threshold
-                __result._pressType = IButton.PressType.Tap;
+                pressType = IButton.PressType.Tap;
             }
 
-            __result.HoldTime = newHoldTime;
+            holdTime = newHoldTime;
 #if DEBUG
             Plugin.Log.LogInfo(
-                $"Setting {nameof(KeyBindings.UI.Crafting.CraftOne)} - {__result.ActionName} prompt to {newHoldTime} (was {originalHoldTime})");
+                $"Setting {nameof(KeyBindings.UI.Crafting.CraftOne)} - {name} prompt to {newHoldTime} (was {originalHoldTime})");
 #endif
         }
 #if DEBUG
         else
         {
-            Plugin.Log.LogInfo($"{nameof(Prompt.Hold)} - Got new Prompt action, HoldTime={__result.HoldTime}, " +
-                               $"name={__result._name}, keybind={__result._key.ToString()}");
+            Plugin.Log.LogInfo($"{nameof(Prompt.Hold)} - Got new Prompt action, HoldTime={holdTime}, " +
+                               $"name={name}, keybind={key}");
         }
 #endif
     }
