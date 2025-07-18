938f0dd31f3dd09ffa746dc0308c852e08091ff9
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-07-18 15:40:17 +0300

    Bump version to 2.0.0

--- UnlimitedOriginPotions/PluginConsts.cs
+++ UnlimitedOriginPotions/PluginConsts.cs
@@ -4,5 +4,5 @@ public static class PluginConsts
 {
     public const string PLUGIN_GUID = "jonanoj.UnlimitedOriginPotions";
     public const string PLUGIN_NAME = "UnlimitedOriginPotions";
-    public const string PLUGIN_VERSION = "1.0.0";
+    public const string PLUGIN_VERSION = "2.0.0";
 }
