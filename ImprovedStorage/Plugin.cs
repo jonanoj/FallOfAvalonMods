671595a5445191419855937b84d8fa2e4ef23c91
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-07-19 17:42:57 +0300

    Generate ImprovedStorage mod from template

---
+++ ImprovedStorage/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ImprovedStorage;
+
+[BepInPlugin(PluginConsts.PLUGIN_GUID, PluginConsts.PLUGIN_NAME, PluginConsts.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log;
+    internal static PluginConfig PluginConfig;
+
+    public Harmony HarmonyInstance { get; set; }
+
+    public void Awake()
+    {
+        Log = Logger;
+        Log.LogInfo($"Plugin {PluginConsts.PLUGIN_GUID} is loading...");
+
+        PluginConfig = new PluginConfig(Config);
+        HarmonyInstance = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+        Log.LogInfo($"Plugin {PluginConsts.PLUGIN_GUID} is loaded!");
+    }
+
+    public void OnDestroy()
+    {
+        Log.LogInfo($"Plugin {PluginConsts.PLUGIN_GUID} is unloading...");
+
+        HarmonyInstance?.UnpatchSelf();
+
+        Log.LogInfo($"Plugin {PluginConsts.PLUGIN_GUID} is unloaded!");
+    }
+}
---
+++ ImprovedStorage/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace ImprovedStorage;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.ImprovedStorage";
+    public const string PLUGIN_NAME = "ImprovedStorage";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
