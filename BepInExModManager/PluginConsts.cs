c661551e31a71084c391d7c02ed8d1c1d5d415c8
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-07-19 15:29:36 +0300

    Generate BepInExModManager mod from template

---
+++ BepInExModManager/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace BepInExModManager;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.BepInExModManager";
+    public const string PLUGIN_NAME = "BepInExModManager";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
