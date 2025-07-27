455a2f37096c88aab6d9385aaee422c9fe2c0028
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-07-27 20:39:43 +0300

    Generate HandRegrow mod from template

---
+++ HandRegrow/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace HandRegrow;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.HandRegrow";
+    public const string PLUGIN_NAME = "HandRegrow";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
