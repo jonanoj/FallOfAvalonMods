063e66ade2af1b5ffc2047117a94831de2ce8c12
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-07-31 22:14:01 +0300

    Generate CustomDifficulty mod from template

---
+++ CustomDifficulty/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace CustomDifficulty;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.CustomDifficulty";
+    public const string PLUGIN_NAME = "CustomDifficulty";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
