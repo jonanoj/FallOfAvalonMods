fc13e3cd69431364353e90d2d5e98e80fff30c7c
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-08-09 18:09:56 +0300

    Generate DualTwoHanded mod from template

---
+++ DualTwoHanded/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace DualTwoHanded;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.DualTwoHanded";
+    public const string PLUGIN_NAME = "DualTwoHanded";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
