8335058a888e228667f038a9fb8bad52f108066c
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-08-10 23:06:12 +0300

    Generate PersistentBgm mod from template

---
+++ PersistentBgm/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace PersistentBgm;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.PersistentBgm";
+    public const string PLUGIN_NAME = "PersistentBgm";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
