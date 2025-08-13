e13a6f383f1659807693479df2da12e9f465af97
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-08-13 23:48:50 +0300

    Generate DamageNumbers mod from template

---
+++ DamageNumbers/PluginConsts.cs
@@ -0,0 +1,8 @@
+namespace DamageNumbers;
+
+public static class PluginConsts
+{
+    public const string PLUGIN_GUID = "jonanoj.DamageNumbers";
+    public const string PLUGIN_NAME = "DamageNumbers";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
