09b0b425d9a107f269a4028a8ce172539dd73d69
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-08-16 18:14:38 +0300

    Bump version to 1.1.0

--- DualTwoHanded/PluginConsts.cs
+++ DualTwoHanded/PluginConsts.cs
@@ -4,5 +4,5 @@ public static class PluginConsts
 {
     public const string PLUGIN_GUID = "jonanoj.DualTwoHanded";
     public const string PLUGIN_NAME = "DualTwoHanded";
-    public const string PLUGIN_VERSION = "1.0.0";
+    public const string PLUGIN_VERSION = "1.1.0";
 }
