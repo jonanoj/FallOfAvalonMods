918c7794409153a525732c6b2bb66e2d9bfc6c6f
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-08-20 23:34:42 +0300

    Bump version to 2.0.1

--- CustomDifficulty/PluginConsts.cs
+++ CustomDifficulty/PluginConsts.cs
@@ -4,5 +4,5 @@ public static class PluginConsts
 {
     public const string PLUGIN_GUID = "jonanoj.CustomDifficulty";
     public const string PLUGIN_NAME = "CustomDifficulty";
-    public const string PLUGIN_VERSION = "2.0.0";
+    public const string PLUGIN_VERSION = "2.0.1";
 }
