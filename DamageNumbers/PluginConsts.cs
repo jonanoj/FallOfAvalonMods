870043c2029e292490d0bc78f5811a253b67e716
jonanoj <4640274+jonanoj@users.noreply.github.com>
2025-10-09 00:41:07 +0300

    Bump version to 1.2.0

--- DamageNumbers/PluginConsts.cs
+++ DamageNumbers/PluginConsts.cs
@@ -4,5 +4,5 @@ public static class PluginConsts
 {
     public const string PLUGIN_GUID = "jonanoj.DamageNumbers";
     public const string PLUGIN_NAME = "DamageNumbers";
-    public const string PLUGIN_VERSION = "1.1.0";
+    public const string PLUGIN_VERSION = "1.2.0";
 }
